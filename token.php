4c3ebd5067a7681b28b9af81360c38e2d7e4b5b2
naveen <naveen.goud@karmanya.co.in>
2016-04-20 15:35:54 +0530

    CrossSite Request Forgery

---
+++ token.php
@@ -0,0 +1,16 @@
+<?php
+class First{
+    public static function generate() {
+       return $_SESSION['token']= base64_encode(openssl_random_pseudo_bytes(32));
+}
+public static function check($token) {
+    if(isset($_SESSION['token']) && $token==$_SESSION['token'])
+    {
+     unset($_SESSION['token']);
+        return false;
+    }
+    return ture;
+}
+
+}
+?>
